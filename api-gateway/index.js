6ff2770a0c8798790186d93e8b9a85ddc3c6e526
Márcio Barbosa <arte.marciobarbosa@gmail.com>
2019-05-04 13:55:55 -0300

    Ajustes na api

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -22,6 +22,13 @@ const productsServiceProxy = httpProxy('http://localhost:3002');
 
 function verifyJWT(req, res, next){
   const token = req.headers['x-access-token'];
+
+  /**
+   * 
+   * Montar uma solução para verificar se o jwt está na blacklist.
+   * 
+   */
+
   if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
   
   jwt.verify(token, process.env.SECRET, function(err, decoded) {
@@ -33,30 +40,57 @@ function verifyJWT(req, res, next){
 }
 
 app.post('/login', (req, res, next) => {
+  /**
+   * Montar solução para ver se o usuario existe no banco
+   * 
+   */
   if(req.body.user === 'marcio' && req.body.pwd === '123'){
 
     const id = 1; //esse id viria do banco de dados
+
     const token = jwt.sign({ id }, process.env.SECRET, {
-      expiresIn: 300 // expires in 5min
+      expiresIn: 864000 // expires in 1d
     });
     res.status(200).send({ auth: true, token: token });
   }
-  
+  /**
+   * Montar uma solução caso o usuario não exista no banco
+   * deverá redirecionar a tela de login
+   * 
+   */
   res.status(500).send('Login inválido!');
 });
 
 app.post('/logout', function(req, res) {
+  /**
+   * 
+   * Montar uma solução para incluir o token na blacklist de jwt's
+   * 
+   */
   res.status(200).send({ auth: false, token: null });
 });
 
 // Proxy request
-app.get('/users', verifyJWT, (req, res, next) => {
+app.get('/api/users', verifyJWT, (req, res, next) => {
   userServiceProxy(req, res, next);
 });
 
-app.get('/products', verifyJWT, (req, res, next) => {
-  productsServiceProxy(req, res, next);
+app.get('/api/user/:id', verifyJWT, (req, res, next) => {
+  userServiceProxy(req, res, next);
 });
+
+app.post('/api/user', verifyJWT, (req, res, next) => {
+  userServiceProxy(req, res, next);
+});
+
+app.put('/api/user/:id', verifyJWT, (req, res, next) => {
+  userServiceProxy(req, res, next);
+});
+
+app.delete('/api/user/:id', verifyJWT, (req, res, next) => {
+  userServiceProxy(req, res, next);
+});
+
 
 const server = http.createServer(app);
 server.listen(3000);
--- api-users/routes/index.js
+++ api-users/routes/index.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/users', function(req, res, next) {
-  res.json([{id:1, nome: 'Luiz', idade: 29},{id:2, nome: 'Fernando', idade: 29}]);
+router.get('/api/users', function(req, res, next) {
+  res.json({ message: `Visualizar todos os Usuarios` });
+});
+
+router.post('/api/user', function(req, res, next) {
+  res.json({ message: `Criar um novo usuario` });
+});
+
+router.get('/api/user/:id', function(req, res, next) {
+  // req.params.id
+  res.json({ message: `Visualizar Usuario id ${ req.params.id }` });
+});
+
+router.put('/api/user/:id', function(req, res, next) {
+  // req.params.id
+  res.json({ message: `Editar Usuario id ${ req.params.id }` });
+});
+
+router.delete('/api/user/:id', function(req, res, next) {
+  // req.params.id
+  res.json({ message: `Deletar Usuario id ${ req.params.id }` });
 });
 
 module.exports = router;
